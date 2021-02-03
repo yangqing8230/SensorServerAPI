2693004194949a05a90e60b020e3ec6befb899d3
yangqing <15275228230@163.com>
2021-02-03 16:50:47 +0800

    接口编译增加对Python的支持，TaskType类型调整

--- compile.bat
+++ compile.bat
@@ -1,13 +1,19 @@
 set PROTO_INCLUDE_DIR=D:\vcpkg\installed\x64-windows\include\
-set GPRC_CPP_PLUGIN="D:\vcpkg\installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe"
 set COMPILE_TOOL_SET=.\tools\vs2019\
 set CPP_GENERATED=.\generated_cpp
-
+set PYTHON_GENERATED=.\generated_python
 set PROTO_FILES_TO_COMPILE=.\sensor.proto .\scan.proto .\pscan.proto .\discrete_scan.proto .\IF_scan.proto .\analog_demod.proto .\iq_acquire.proto .\drone_detect.proto .\tdoa.proto
 
 @rem cpp
+set GPRC_CPP_PLUGIN="D:\vcpkg\installed\x64-windows\tools\grpc\grpc_cpp_plugin.exe"
 
 %COMPILE_TOOL_SET%protoc --proto_path=%PROTO_INCLUDE_DIR% --proto_path=.\  --grpc_out=%CPP_GENERATED%   --plugin=protoc-gen-grpc=%GPRC_CPP_PLUGIN%  %PROTO_FILES_TO_COMPILE%
 
 %COMPILE_TOOL_SET%protoc --proto_path=%PROTO_INCLUDE_DIR% --proto_path=.\  --cpp_out=%CPP_GENERATED%  %PROTO_FILES_TO_COMPILE%
 
+@rem python
+set GPRC_PYTHON_PLUGIN="D:\vcpkg\installed\x64-windows\tools\grpc\grpc_python_plugin.exe"
+
+%COMPILE_TOOL_SET%protoc --proto_path=%PROTO_INCLUDE_DIR% --proto_path=.\  --grpc_python_out=%PYTHON_GENERATED%  --plugin=protoc-gen-grpc_python=%GPRC_PYTHON_PLUGIN%  %PROTO_FILES_TO_COMPILE%
+
+%COMPILE_TOOL_SET%protoc --proto_path=%PROTO_INCLUDE_DIR% --proto_path=.\  --python_out=%PYTHON_GENERATED% %PROTO_FILES_TO_COMPILE%
